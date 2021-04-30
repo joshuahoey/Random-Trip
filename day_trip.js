95c052102ab71101d94d03e410b44af510236491
GingerGuy93 <alwayssunny93@gmail.com>
2021-04-30 16:06:40 -0400

    Updated with psudocode and functions written out

--- day_trip.js
+++ day_trip.js
@@ -1,4 +1,35 @@
+'use strict';
 const colors = [ 'blue', 'yellow', 'orange', 'red' ];
 const randIndex = Math.floor(Math.random() * colors.length);
 
 console.log(colors[randIndex]);
+
+//random destination
+function destination(placesArr){
+
+}
+let randomDestination = destination(["Boston", "NYC", "Chicago", "Houston", "San Fransisco", "DC", "Tampa Bay"])
+
+//random restaurant
+function restaurant(foodArr){
+
+}
+
+let randomFood = restaurant(["5 Star", "Pizza", "Seafood", "Spicy", "Ramen", "Italian",])
+//random transportation
+function transportation(transArr){
+
+}
+
+let randomTransport = transportation(["Car", "Bus", "Bike", "Train", "Bike"])
+//random entertainment
+function entertainment(funArr){
+
+}
+
+let randomEntertainment = entertainment(["Concert", "Movie", "Hike", "Bar Hopping"])
+//function that displays all the randoms 
+//prompt user if they like the trip
+//if yes then console.log trip
+//else generate new trip
+
