b42ce9ba77a8a4abe9e41eaeca46a4bbf10d72cb
GingerGuy93 <alwayssunny93@gmail.com>
2021-04-30 16:17:46 -0400

    Updated with functions for all random things for the trip

--- day_trip.js
+++ day_trip.js
@@ -6,28 +6,36 @@ console.log(colors[randIndex]);
 
 //random destination
 function destination(placesArr){
-
+    let randomPlace =  Math.floor(Math.random() * placesArr.length);
+    return placesArr[randomPlace];
 }
 let randomDestination = destination(["Boston", "NYC", "Chicago", "Houston", "San Fransisco", "DC", "Tampa Bay"])
+console.log(randomDestination);
 
 //random restaurant
 function restaurant(foodArr){
-
+    let randomRestaurant = Math.floor(Math.random() * foodArr.length);
+    return foodArr[randomRestaurant];
 }
-
 let randomFood = restaurant(["5 Star", "Pizza", "Seafood", "Spicy", "Ramen", "Italian",])
+console.log(randomFood);
+
 //random transportation
 function transportation(transArr){
-
+    let randomTransportation = Math.floor(Math.random() * transArr.length);
+    return transArr[randomTransportation];
 }
-
 let randomTransport = transportation(["Car", "Bus", "Bike", "Train", "Bike"])
+console.log(randomTransport);
+
 //random entertainment
 function entertainment(funArr){
-
+    let randomFun = Math.floor(Math.random() * funArr.length);
+    return funArr[randomFun];
 }
-
 let randomEntertainment = entertainment(["Concert", "Movie", "Hike", "Bar Hopping"])
+console.log(randomEntertainment);
+
 //function that displays all the randoms 
 //prompt user if they like the trip
 //if yes then console.log trip
